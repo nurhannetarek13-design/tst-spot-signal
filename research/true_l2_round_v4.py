849bf6b2f89e9a6d9d36d49488b24e0700f5ba5c
nurhannetarek13-design <nurhanne.tarek13@gmail.com>
2026-09-10 20:37:56 +0300

    Trigger corrected September L2 research

--- research/true_l2_round_v4.py
+++ research/true_l2_round_v4.py
@@ -3,6 +3,7 @@
 import sys
 import research.true_l2_round_v3 as v3
 
+# Corrected collector generation: crypto-lob-stream >=0.9.2.
 v3.DATASET='Goooddy/crypto-lob-stream'
 v3.BASE='https://huggingface.co/datasets/Goooddy/crypto-lob-stream/resolve/main/'
 v3.MONTH='2026-09'
