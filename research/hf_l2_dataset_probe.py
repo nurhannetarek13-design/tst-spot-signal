39cd9af8c4d6a712106cec748bdf4cb999df668d
nurhannetarek13-design <nurhanne.tarek13@gmail.com>
2026-09-10 18:31:23 +0300

    Probe Hugging Face L2 file sizes

--- research/hf_l2_dataset_probe.py
+++ research/hf_l2_dataset_probe.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import json, urllib.request
 DATASET='MaximumLeverage/crypto-lob-stream'
-UA='tst-hf-l2-probe/1.0'
+UA='tst-hf-l2-probe/1.1'
 
 def get_json(url):
     req=urllib.request.Request(url,headers={'User-Agent':UA})
@@ -9,7 +9,9 @@ def get_json(url):
         return json.load(r)
 
 info=get_json('https://huggingface.co/api/datasets/'+DATASET)
+tree=get_json('https://huggingface.co/api/datasets/'+DATASET+'/tree/main?recursive=true&expand=true')
 files=[]
-for s in info.get('siblings',[]):
-    files.append({'rfilename':s.get('rfilename'),'size':s.get('size'),'blob_id':s.get('blobId'),'lfs':s.get('lfs')})
+for s in tree:
+    if s.get('type')=='file':
+        files.append({'path':s.get('path'),'size':s.get('size'),'oid':s.get('oid')})
 print(json.dumps({'dataset':DATASET,'private':info.get('private'),'gated':info.get('gated'),'downloads':info.get('downloads'),'files':files},indent=2))
