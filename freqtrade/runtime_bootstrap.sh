60477639ee75f3ab55d9e565bbb351314fb8d5a2
nurhannetarek13-design <nurhanne.tarek13@gmail.com>
2026-09-14 16:06:02 +0300

    Start Alligator SMC paper runner on Railway

--- freqtrade/runtime_bootstrap.sh
+++ freqtrade/runtime_bootstrap.sh
@@ -9,4 +9,20 @@ chown -R ftuser:ftuser /data
 chmod 0750 /data
 
 echo "[runtime-bootstrap] persistent /data prepared; dropping to ftuser"
-exec su -s /bin/bash ftuser -c 'python -u /freqtrade/front_proxy.py & exec /bin/bash /freqtrade/entrypoint.sh'
+exec su -s /bin/bash ftuser -c '
+if [[ "${ALLIGATOR_SMC_PAPER_ENABLED:-1}" == "1" ]]; then
+  (
+    while true; do
+      echo "[runtime-bootstrap] starting TST_ALLIGATOR_SMC_V2 paper runner"
+      python -u /freqtrade/alligator_smc_paper_runner.py
+      rc=$?
+      echo "[runtime-bootstrap] paper runner exited rc=${rc}; restarting in 5s" >&2
+      sleep 5
+    done
+  ) &
+  echo "[runtime-bootstrap] Alligator SMC paper runner enabled"
+else
+  echo "[runtime-bootstrap] Alligator SMC paper runner disabled"
+fi
+exec /bin/bash /freqtrade/entrypoint.sh
+'
